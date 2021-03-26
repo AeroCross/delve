86ad2a54039511da70c3cdcf25bdcd324b5721d7
Mario Cuba <mario.cuba@envato.com>
2021-03-26 12:03:10 +1100

    Provide more specs for common use-cases

--- spec/model/model_spec.rb
+++ spec/model/model_spec.rb
@@ -21,13 +21,31 @@ RSpec.describe Model do
   describe "#find_by" do
     subject { Model.new(data).find_by(key, value) }
 
-    context "when providing a correct key and value" do
+    context "when providing a matching key and value" do
       let(:key) { :email }
       let(:value) { "foo@bar.com" }
 
       it "returns an array with the results" do
         expect(subject.length).to eq(1)
         expect(subject[0]).to include({ name: "Foobar" })
+      end
+    end
+
+    context "when providing a key that does not exist" do
+      let(:key) { :superpowers }
+      let(:value) { "Flying" }
+
+      it "returns an empty array" do
+        expect(subject).to be_empty
+      end
+    end
+
+    context "when providing a key that exists but the value does not match" do
+      let(:key) { :email }
+      let(:value) { "danan-theman@vic.gov.au" }
+
+      it "returns an empty array" do
+        expect(subject).to be_empty
       end
     end
   end
