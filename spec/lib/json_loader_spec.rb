6a64989416136fdd40ec842538336643bd8516fc
Mario Cuba <mario.cuba@envato.com>
2021-03-26 19:58:21 +1100

    The first feature to be supported will be llisting all the keys in the data set
    This is the work necessary to get something outputted to the CLI once the app is run

---
+++ spec/lib/json_loader_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "byebug"
+require_relative "../../lib/json_loader.rb"
+
+RSpec.describe JSONLoader do
+  subject { JSONLoader.call(json) }
+
+  let(:json) do
+    [
+      {
+        a: "foo",
+        b: "bar",
+        c: "baz",
+      },
+      {
+        c: "fizz",
+        d: "buzz",
+      },
+    ].to_json
+  end
+
+  context "when valid JSON is passed to it" do
+    it "loads a ruby data structure" do
+      expect(subject.length).to eq(2)
+      expect(subject[0]).to eq({ a: "foo", b: "bar", c: "baz" })
+    end
+  end
+
+  describe "#all_keys" do
+    subject { JSONLoader.new(json).all_keys }
+    it "returns all the unique keys found at the top level of the data array" do
+      expect(subject).to eq([:a, :b, :c, :d])
+    end
+  end
+end
