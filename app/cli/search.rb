5e844c3ea50a93f66c2853f273e0d4eebd1b7f66
Mario Cuba <mario.cuba@envato.com>
2021-03-27 13:53:00 +1100

    Allow the results of the commands to be passed to the renderers
    This means that they no longer print anything.

--- app/cli/search.rb
+++ app/cli/search.rb
@@ -37,14 +37,7 @@ module CLI
       return false if source.nil?
 
       @data = Loader.json_file(file_to_load(source[:name]))
-      result = source[:model].new(data.data).find_by(field.to_sym, cast(value))
-
-      if result.empty?
-        puts "No results found"
-      end
-
-      puts result
-      return result
+      source[:model].new(data.data).find_by(field.to_sym, cast(value))
     end
 
     private
