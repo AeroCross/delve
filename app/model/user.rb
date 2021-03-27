be03cddc266bf5516fe5fe1c87ba76d666619199
Mario Cuba <mario.cuba@envato.com>
2021-03-27 20:03:59 +1100

    Define data in the User model to help identify what are key fields

--- app/model/user.rb
+++ app/model/user.rb
@@ -3,4 +3,15 @@
 require_relative "./model"
 
 class User < Model
+  def primary_key
+    :"_id"
+  end
+
+  def unique_keys
+    %w(_id)
+  end
+
+  def foreign_keys
+    %w(organization_id)
+  end
 end
